b357b93a7fe79633fb8fbd2508f694792eb01322
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-04 21:50:06 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,11 +23,12 @@ class ProductsController < ApplicationController
   end
 
   def show
+
     @product = Product.find(params[:id])
     respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
-    end
+  format.html
+  format.json {render json: @product }
+end
   end
 
   private
